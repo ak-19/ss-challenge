b091c32751c537648c31e2284f5b7949ed1fccf7
ante <ante.kotarac@gmail.com>
2023-08-06 19:45:38 +0200

    call within index file example

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { FindLettersAndPath } from "./FindLettersAndPath";
+
+const a =
+    ['@---A---+',
+        '        |',
+        'x-B-+   C',
+        '    |   |',
+        '    +---+'];
+
+const finder = new FindLettersAndPath(a);
+
+finder.process();
+
+const { letters, path } = finder.get();
+
+console.log(letters, path);
